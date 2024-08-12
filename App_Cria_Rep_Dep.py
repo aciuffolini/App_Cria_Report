b8ed1ffcdd83fcb43ef870d2cc4e8c5291ed1ecc
Atilio Ciuffolini <106196184+aciuffolini@users.noreply.github.com>
2024-08-12 10:03:01 -0300

    Update App_Cria_Rep_Dep.py

--- App_Cria_Rep_Dep.py
+++ App_Cria_Rep_Dep.py
@@ -11,7 +11,8 @@ from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 #from langchain_community.llms import OllamaLLM
 #from langchain_ollama.llms import OllamaLLM
-from langchain_openai import ChatOpenAI
+#from langchain_openai import ChatOpenAI
+from langchain.llms import ChatOpenAI
 from langchain_ollama import ChatOllama
 from langchain.llms import OpenAI
 
